8d85462f9a86cf0284c05723c43b8f2d6652efae
lucunyu <lucunyu@cloudwalk.cn>
2019-09-10 01:25:15 +0800

    961,561

---
+++ src/main/java/cc/kevinlu/leetcode/MainTest561.java
@@ -0,0 +1,21 @@
+package cc.kevinlu.leetcode;
+
+import java.util.Arrays;
+
+public class MainTest561 {
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 4, 3, 2 };
+        System.out.println(arrayPairSum(nums));
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+}
