982404acfed1afc654dab60b60ee07efddefab54
lucunyu <417165765@qq.com>
2019-12-23 22:07:17 +0800

    x

--- src/main/java/cc/kevinlu/leetcode/cc/MainTest191.java
+++ src/main/java/cc/kevinlu/leetcode/cc/MainTest191.java
@@ -7,11 +7,8 @@ public class MainTest191 {
         int bitCount = 0;
         while (n != 0) {
             bitCount++;
-            System.out.printf("%d & %d = ", n, n - 1);
             n &= (n - 1);
-            System.out.println(n);
         }
-        System.out.println();
         System.out.println(bitCount);
     }
 
