0561a1251cc7241a9b28c2466c91453f94e2cd57
lucunyu <lucunyu@cloudwalk.cn>
2019-12-26 11:53:53 +0800

    909

--- src/main/java/cc/kevinlu/leetcode/MainTest009.java
+++ src/main/java/cc/kevinlu/leetcode/MainTest009.java
@@ -7,17 +7,14 @@ public class MainTest009 {
     }
 
     public static boolean isPalindrome(int x) {
-        if (x < 0) return false;
-        int result = 0, temp = 0,mid = x;
+        if (x < 0)
+            return false;
+        int result = 0, temp = 0, mid = x;
         while (x != 0) {
             temp = x % 10;
             result = result * 10 + temp;
             x = x / 10;
         }
-        if(result == mid){
-            return true;
-        }else{
-            return false;
-        }
+        return result == mid;
     }
 }
