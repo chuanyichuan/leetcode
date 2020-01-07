8f09030f0dd9332c0d6821adf1995d92d5072318
zero <zero.shan@taxunion.com.cn>
2020-01-07 17:30:26 +0800

    35

---
+++ src/main/java/cc/leetcode/zero/MainTest001.java
@@ -0,0 +1,46 @@
+package cc.leetcode.zero;
+
+import java.util.Arrays;
+
+/**
+ * @author zero
+ * @date 2020/1/7 5:10 下午
+ */
+public class MainTest001 {
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     *
+     * 示例:
+     *
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     *
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     */
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 4, 5, 6};
+        int target = 7;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = nums[i];
+                    result[1] = nums[j];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
